ffd3af9a4af86eb641c62e1e02f9bba8554d894c
Ahmet Yakup Beyaz <109310845+jacoells@users.noreply.github.com>
2025-12-21 16:19:27 +0200

    admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Outline, OutlineVersion, ApprovalLog
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Outline)
+admin.site.register(OutlineVersion)
+admin.site.register(ApprovalLog)
